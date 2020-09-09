afc454be2e522542a5b32e585620e0e36481f41e
Maxim Kokryashkin <max.kokryashkin@gmail.com>
2020-09-09 13:17:37 +0300

    almost finished button class

--- graphics_exp.cpp
+++ graphics_exp.cpp
@@ -7,16 +7,87 @@ const sf::Color SECONDARY_LIGHT = sf::Color(108, 142, 173);
 const sf::Color SECONDARY_DARK = sf::Color(58, 51, 53);
 const sf::Color PRIMARY_ACCENT = sf::Color(255, 58, 32);
 
+const sf::Vector2f SORT_BUTTON_SIZE = sf::Vector2f(120.f, 50.f);
+const unsigned int BUTTON_TEXT_SIZE = 24;
+
+sf::Font ROBOTO_MEDIUM;
+
+
 class rectButton
 {
     public:
         sf::RectangleShape base;
         sf::Text text;
+        
+        void setSize(sf::Vector2f base_size)
+        {
+            base.setSize(base_size);
+        }
+
+        rectButton()
+        {
+            base = sf::RectangleShape();
+            text = sf::Text();
+        }
+
+        void setColor(const sf::Color& base_color)
+        {
+            base.setFillColor(base_color);
+        }
+
+        void setTextColor(const sf::Color& text_color)
+        {
+            text.setFillColor(text_color);
+        }
+
+        void setPosition(const sf::Vector2f& pos)
+        {
+            base.setPosition(pos); // add boundary cases
+            text.setPosition(sf::Vector2f(pos.x + 4, pos.y + 10));
+        }
+
+        void setTextFont(const sf::Font& font)
+        {
+            text.setFont(font);
+        }
+
+        void setTextSize(const unsigned int& text_size)
+        {
+            text.setCharacterSize(text_size);
+        }
+
+        void setTextString(const char* string)
+        {
+            text.setString(string);
+        }
+
+        void render(sf::RenderWindow& window)
+        {
+            window.draw(base);
+            window.draw(text);
+        }
+
 };
+
+rectButton createSortStyledButton(const sf::Vector2f& pos, const char* string)
+{
+        rectButton button;
+
+        button.setColor(PRIMARY_DARK);
+        button.setSize(SORT_BUTTON_SIZE);
+        button.setPosition(pos);
+        button.setTextFont(ROBOTO_MEDIUM);
+        button.setTextColor(PRIMARY_LIGHT);
+        button.setTextSize(BUTTON_TEXT_SIZE);
+        button.setTextString(string);
+
+        return button;
+}
 
 int main() 
 {
     sf::RenderWindow window(sf::VideoMode(1600, 900), "Sorts analyzer");
+    ROBOTO_MEDIUM.loadFromFile("fonts/Roboto-Light.ttf");
 
     while (window.isOpen())
     {
@@ -30,37 +101,12 @@ int main()
         }
 
         window.clear(PRIMARY_LIGHT); // clearing window with black color
-        sf::RectangleShape rect(sf::Vector2f(1600.f, 700.f));
+        sf::RectangleShape rect(sf::Vector2f(1600.f, 700.f)); //leave it here for graphics background
         rect.setFillColor(SECONDARY_DARK);
 
-        /*sf::Font font;
-        if(!font.loadFromFile("fonts/Roboto-Thin.ttf"))
-        {
-            fprintf(stderr, "Error loading fonts");
-        }
-        sf::Text text;
-        text.setFont(font);
-        text.setPosition(50.f, 300.f);
-        text.setFillColor(sf::Color::Red);
-        text.setCharacterSize(24);
-        text.setString("MergeSort");
-        window.draw(text);*/
+        rectButton button = createSortStyledButton(sf::Vector2f(50.f, 750.f), "MergeSort");
 
-        
-        rectButton button = {};
-        button.base = sf::RectangleShape(sf::Vector2f(120.f, 50.f)); //leave it here for graphics background
-        button.base.setFillColor(PRIMARY_DARK);
-        button.base.setPosition(sf::Vector2f(50.f, 750.f));
-        sf::Font font;
-        font.loadFromFile("fonts/Roboto-Light.ttf");
-        button.text.setFont(font);
-        button.text.setFillColor(PRIMARY_LIGHT);
-        button.text.setPosition(sf::Vector2f(54.f, 760.f));
-        button.text.setCharacterSize(24);
-        button.text.setString("MergeSort");
-
-        window.draw(button.base);
-        window.draw(button.text);
+        button.render(window);
         window.draw(rect);
         window.display();
 
