699649f94c53c9258f34bf966c28525c073ec3f7
Maxim Kokryashkin <max.kokryashkin@gmail.com>
2020-09-14 03:21:58 +0300

    optimized benchmark function

--- benchmark/benchmark/service.hpp
+++ benchmark/benchmark/service.hpp
@@ -23,20 +23,18 @@ void generateArray(Stat<int>* begin, Stat<int>* end, size_t mod = 10)
 std::pair<int, int>* benchmarkSort(size_t n_samples, const AbstractSort<Stat<int>>& sort)
 {
     auto results = new std::pair<int, int>[n_samples]();
+    Stat<int>* array = new Stat<int>[n_samples]();
 
     for(size_t current_size = 1; current_size < n_samples; ++current_size) 
     {
         Stat<int>::n_comparisons = 0;
         Stat<int>::n_assignments = 0;
-
-        Stat<int>* array = new Stat<int>[current_size]();
         generateArray(array, array + current_size);
         sort(array, array + current_size);
         results[current_size] = {Stat<int>::n_comparisons, Stat<int>::n_assignments};
-
-        delete[] array;
     }
 
+    delete[] array;
     return results;
 }
 
